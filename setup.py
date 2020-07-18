ccd612a8e65e8bd735a8bdabe6924655a9c4df3a
abaisero <andrea.baisero@gmail.com>
2020-07-18 14:51:37 -0400

    add setup.py and empty source/test folders

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+from gym_gridverse import __version__
+
+setup(
+    name='gym-gridverse',
+    version=__version__,
+    description='Gridworld domains in the gym interface',
+    author='Andrea Baisero',
+    author_email='andrea.baisero@gmail.com',
+    url='https://github.com/abaisero/gym-gridverse',
+    packages=['gym_gridverse'],
+    install_requires=['gym'],
+    license='MIT',
+)
