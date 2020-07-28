893a196c423470183d6ae437583930c4196d8e6b
sammie katt <sammie.katt@gmail.com>
2020-07-28 16:03:49 -0400

    implement and test wall, goal, door & key grid objects

--- gym_gridverse/grid_object.py
+++ gym_gridverse/grid_object.py
@@ -23,23 +23,18 @@ class GridObject(metaclass=abc.ABCMeta):
 
     # registry as list/mapping int -> GridObject
     object_types: List[GridObject] = []
+    type_index: int
 
     def __init_subclass__(cls, *, noregister=False, **kwargs):
         super().__init_subclass__(**kwargs)
         if not noregister:
-            cls._object_type = len(GridObject.object_types)
+            cls.type_index = len(GridObject.object_types)
             GridObject.object_types.append(cls)
 
     @property
-    def object_type(self) -> int:
-        """ Returns the type of the object """
-        # NOTE: magically assumed set through registration
-        return type(self)._object_type
-
-    @property
     @abc.abstractmethod
-    def state(self) -> int:
-        """ Returns the state of the object """
+    def state_index(self) -> int:
+        """ Returns the state index of the object """
 
     @property
     @abc.abstractmethod
@@ -62,23 +57,23 @@ class GridObject(metaclass=abc.ABCMeta):
         """ Whether the object blocks the agent """
 
     @abc.abstractmethod
-    def update(self, state) -> None:
+    def step(self, state) -> None:  # TODO: state annotate?
         """ Optional behavior of the object """
 
     @abc.abstractmethod
-    def actuate(self, state) -> None:
+    def actuate(self, state) -> None:  # TODO: state annotate?
         """ The (optional) behavior upon actuation """
 
     def as_array(self):
         """ A (3,) array representation of the object """
-        return np.array([self.object_type, self.state, int(self.color.value)])
+        return np.array([self.type_index, self.state_index, self.color.value])
 
 
 class Floor(GridObject):
     """ Most basic object in the grid, represents empty cell """
 
     @property
-    def state(self) -> int:
+    def state_index(self) -> int:
         return 0
 
     @property
@@ -97,8 +92,191 @@ class Floor(GridObject):
     def blocks(self) -> bool:
         return False
 
-    def update(self, state) -> None:
+    def step(self, state) -> None:
         pass
 
     def actuate(self, state) -> None:
+        pass
+
+
+class Wall(GridObject):
+    """ The (second) most basic object in the grid: blocking cell """
+
+    @property
+    def state_index(self) -> int:
+        return 0
+
+    @property
+    def color(self) -> Colors:
+        return Colors.NONE
+
+    @property
+    def transparent(self) -> bool:
+        return False
+
+    @property
+    def can_be_picked_up(self) -> bool:
+        return False
+
+    @property
+    def blocks(self) -> bool:
+        return True
+
+    def step(self, state) -> None:
+        pass
+
+    def actuate(self, state) -> None:
+        pass
+
+
+class Goal(GridObject):
+    """ The (second) most basic object in the grid: blocking cell """
+
+    @property
+    def state_index(self) -> int:
+        return 0
+
+    @property
+    def color(self) -> Colors:
+        return Colors.NONE
+
+    @property
+    def transparent(self) -> bool:
+        return True
+
+    @property
+    def can_be_picked_up(self) -> bool:
+        return False
+
+    @property
+    def blocks(self) -> bool:
+        return False
+
+    def step(self, state) -> None:
+        pass
+
+    def actuate(self, state) -> None:
+        pass
+
+
+class Door(GridObject):
+    """ A door is a grid
+
+    Can be `open`, `closed` or `locked`.
+
+    The following dynamics (upon actuation) occur:
+
+    When not holding correct key with correct color:
+        `open` or `closed` -> `open`
+        `locked` -> `locked`
+
+    When holding correct key:
+        any state -> `open`
+
+    """
+
+    class State(enum.Enum):
+        """ open, closed or locked """
+
+        OPEN = 0
+        CLOSED = enum.auto()
+        LOCKED = enum.auto()
+
+    def __init__(self, state: State, color: Colors):
+        self._color = color
+        self._state = state
+
+    @property
+    def state_index(self) -> int:
+        return self._state.value
+
+    @property
+    def color(self) -> Colors:
+        return self._color
+
+    @property
+    def transparent(self) -> bool:
+        return self._state == self.State.OPEN
+
+    @property
+    def can_be_picked_up(self) -> bool:
+        return False
+
+    @property
+    def blocks(self) -> bool:
+        return not self._state == self.State.OPEN
+
+    def step(self, state) -> None:
+        pass
+
+    def actuate(self, state) -> None:
+        """ Attempts to open door 
+
+        When not holding correct key with correct color:
+            `open` or `closed` -> `open`
+            `locked` -> `locked`
+
+        When holding correct key:
+            any state -> `open`
+
+        """
+
+        if self.is_open:
+            return
+        if not self.locked:
+            self._state = self.State.OPEN
+        else:
+            try:
+                # TODO: test through type index or isinstance?
+                if (
+                    isinstance(state.agent.obj, Key)
+                    and state.agent.obj.color == self.color
+                ):
+                    self._state = self.State.OPEN
+                # TODO: disappear key?
+            except:
+                pass
+
+    @property
+    def is_open(self) -> bool:
+        """ returns whether the door is opened """
+        return self._state == self.State.OPEN
+
+    @property
+    def locked(self) -> bool:
+        """ returns whether the door is locked """
+        return self._state == self.State.LOCKED
+
+
+class Key(GridObject):
+    """ A key opens a door with the same color """
+
+    def __init__(self, c: Colors):
+        """ Creates a key of color `c` """
+        self._color = c
+
+    @property
+    def state_index(self) -> int:
+        return 0
+
+    @property
+    def color(self) -> Colors:
+        return self._color
+
+    @property
+    def transparent(self) -> bool:
+        return True
+
+    @property
+    def can_be_picked_up(self) -> bool:
+        return True
+
+    @property
+    def blocks(self) -> bool:
+        return False
+
+    def step(self, state) -> None:  # TODO: state annotate?
+        pass
+
+    def actuate(self, state) -> None:  # TODO: state annotate?
         pass
--- tests/test_grid_objects.py
+++ tests/test_grid_objects.py
@@ -1,27 +1,44 @@
 """ Tests Grid Object behavior and properties """
 import unittest
 
+import gym_gridverse.state as state
 import numpy as np
-
-from gym_gridverse.grid_object import Colors, Floor, GridObject
+from gym_gridverse.grid_object import (Colors, Door, Floor, Goal, GridObject,
+                                       Key, Wall)
 
 
 class TestGridObject(unittest.TestCase):
+    """ Tests registering Grid Objects """
+
     def test_no_registration(self):
+        """ Grid Objects can be _not_ registered """
+
         class DummyObject(  # pylint: disable=abstract-method
             GridObject, noregister=True
         ):
-            pass
+            """ Some dummy grid objet that is _not_ registered """
 
         self.assertNotIn(DummyObject, GridObject.object_types)
+
+
+def simple_state_without_object() -> state.State:
+    """ Returns a 2x2 (empty) grid with an agent without an item 
+
+    TODO: Orientation?
+    """
+    return state.State(
+        state.Grid(height=2, width=2),
+        state.Agent(position=(0, 0), orientation=None, obj=Floor()),
+    )
 
 
 class TestFloor(unittest.TestCase):
     """ Basic stupid tests for floor grid object """
 
     def test_registration(self):
+        """ Tests the registration as a Grid Object """
         self.assertIn(Floor, GridObject.object_types)
-        self.assertEqual(Floor._object_type, 0)
+        self.assertEqual(Floor.type_index, 0)
 
     def test_properties(self):
         """ Basic stupid tests for floor grid object """
@@ -32,12 +49,195 @@ class TestFloor(unittest.TestCase):
         self.assertFalse(floor.blocks)
         self.assertEqual(floor.color, Colors.NONE)
         self.assertFalse(floor.can_be_picked_up)
-        self.assertEqual(floor.state, 0)
+        self.assertEqual(floor.state_index, 0)
 
         expected_arr_represtation = np.array([0, 0, 0])
         np.testing.assert_array_equal(
             floor.as_array(), expected_arr_represtation
         )
+
+
+class TestWall(unittest.TestCase):
+    """ Basic stupid tests for wall grid object """
+
+    def test_registration(self):
+        """ Tests the registration as a Grid Object """
+        self.assertIn(Wall, GridObject.object_types)
+        self.assertEqual(Wall.type_index, 1)
+
+    def test_properties(self):
+        """ Basic property tests """
+
+        wall = Wall()
+
+        self.assertEqual(wall.type_index, 1)
+        self.assertFalse(wall.transparent)
+        self.assertTrue(wall.blocks)
+        self.assertEqual(wall.color, Colors.NONE)
+        self.assertFalse(wall.can_be_picked_up)
+        self.assertEqual(wall.state_index, 0)
+
+        expected_arr_represtation = np.array([1, 0, 0])
+        np.testing.assert_array_equal(
+            wall.as_array(), expected_arr_represtation
+        )
+
+
+class TestGoal(unittest.TestCase):
+    """ Basic stupid tests for goal grid object """
+
+    def test_registration(self):
+        """ Tests the registration as a Grid Object """
+        self.assertIn(Goal, GridObject.object_types)
+        self.assertEqual(Goal.type_index, 2)
+
+    def test_properties(self):
+        """ Basic property tests """
+
+        goal = Goal()
+
+        self.assertTrue(goal.transparent)
+        self.assertFalse(goal.blocks)
+        self.assertEqual(goal.color, Colors.NONE)
+        self.assertFalse(goal.can_be_picked_up)
+        self.assertEqual(goal.state_index, 0)
+
+        expected_arr_represtation = np.array([2, 0, 0])
+        np.testing.assert_array_equal(
+            goal.as_array(), expected_arr_represtation
+        )
+
+
+class TestKey(unittest.TestCase):
+    """ Basic stupid tests for Key grid object """
+
+    def test_registration(self):
+        """ Tests the registration as a Grid Object """
+        self.assertIn(Key, GridObject.object_types)
+        self.assertEqual(Key.type_index, 4)
+
+    def test_properties(self):
+        """ Basic property tests """
+
+        color = Colors.YELLOW
+        key = Key(color)
+
+        self.assertTrue(key.transparent)
+        self.assertFalse(key.blocks)
+        self.assertEqual(key.color, color)
+        self.assertTrue(key.can_be_picked_up)
+        self.assertEqual(key.state_index, 0)
+
+        expected_arr_represtation = np.array([4, 0, color.value])
+        np.testing.assert_array_equal(key.as_array(), expected_arr_represtation)
+
+
+class TestDoor(unittest.TestCase):
+    """ Tests for the door grid object """
+
+    def test_registration(self):
+        """ Tests the registration as a Grid Object """
+        self.assertIn(Door, GridObject.object_types)
+        self.assertEqual(Door.type_index, 3)
+
+    def test_open_door_properties(self):
+        """ Basic property tests """
+
+        color = Colors.GREEN
+
+        open_door = Door(Door.State.OPEN, color)
+
+        self.assertTrue(open_door.transparent)
+        self.assertEqual(open_door.color, color)
+        self.assertFalse(open_door.can_be_picked_up)
+        self.assertEqual(open_door.state_index, Door.State.OPEN.value)
+        self.assertTrue(open_door.is_open)
+        self.assertFalse(open_door.locked)
+        self.assertFalse(open_door.blocks)
+
+        expected_arr_represtation = np.array([3, 0, color.value])
+        np.testing.assert_array_equal(
+            open_door.as_array(), expected_arr_represtation
+        )
+
+    def test_closed_door_properties(self):
+        """ Basic property tests """
+
+        color = Colors.NONE
+
+        closed_door = Door(Door.State.CLOSED, color)
+
+        self.assertFalse(closed_door.transparent)
+        self.assertEqual(closed_door.color, color)
+        self.assertFalse(closed_door.can_be_picked_up)
+        self.assertEqual(closed_door.state_index, Door.State.CLOSED.value)
+        self.assertFalse(closed_door.is_open)
+        self.assertFalse(closed_door.locked)
+        self.assertTrue(closed_door.blocks)
+
+        expected_arr_represtation = np.array([3, 1, color.value])
+        np.testing.assert_array_equal(
+            closed_door.as_array(), expected_arr_represtation
+        )
+
+    def test_locked_door_properties(self):
+        """ Basic property tests """
+
+        color = Colors.NONE
+
+        locked_door = Door(Door.State.LOCKED, color)
+
+        self.assertFalse(locked_door.transparent)
+        self.assertEqual(locked_door.color, color)
+        self.assertFalse(locked_door.can_be_picked_up)
+        self.assertEqual(locked_door.state_index, Door.State.LOCKED.value)
+        self.assertFalse(locked_door.is_open)
+        self.assertTrue(locked_door.locked)
+        self.assertTrue(locked_door.blocks)
+
+        expected_arr_represtation = np.array([3, 2, color.value])
+        np.testing.assert_array_equal(
+            locked_door.as_array(), expected_arr_represtation
+        )
+
+    def test_opening_door(self):
+        """ Testing the simple FNS of the door """
+
+        color = Colors.GREEN
+        state = None
+
+        door = Door(Door.State.CLOSED, color)
+        self.assertFalse(door.is_open)
+
+        door.actuate(state)
+        self.assertTrue(door.is_open)
+
+        door.actuate(state)
+        self.assertTrue(door.is_open)
+
+    def test_opening_door_with_key(self):
+        """ Testing the simple FNS of the door """
+
+        color = Colors.BLUE
+
+        # Agent holding wrong key
+        state = simple_state_without_object()
+        state.agent.obj = Key(Colors.YELLOW)
+
+        door = Door(Door.State.LOCKED, color)
+        self.assertFalse(door.is_open)
+
+        door.actuate(state)
+        self.assertFalse(door.is_open)
+
+        state.agent.obj = Key(color)
+        self.assertFalse(door.is_open)
+
+        door.actuate(state)
+        self.assertTrue(door.is_open)
+
+        door.actuate(state)
+        self.assertTrue(door.is_open)
 
 
 if __name__ == '__main__':
