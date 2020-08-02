c53921edbcdcbaddfe58fcea5443dc3ed2df6821
abaisero <andrea.baisero@gmail.com>
2020-08-02 15:17:24 -0400

    add overlap terminating function and decompose reach_goal terminating function

--- gym_gridverse/envs/terminating_functions.py
+++ gym_gridverse/envs/terminating_functions.py
@@ -1,17 +1,34 @@
-from typing import Callable
+from typing import Callable, Type
 
 from gym_gridverse.envs import Actions
-from gym_gridverse.grid_object import Goal
+from gym_gridverse.grid_object import Goal, GridObject
 from gym_gridverse.state import State
 
 TerminatingFunction = Callable[[State, Actions, State], bool]
 
 
-def reach_goal(
+def overlap(
     state: State,  # pylint: disable=unused-argument
     action: Actions,  # pylint: disable=unused-argument
     next_state: State,
+    *,
+    object_type: Type[GridObject],
 ) -> bool:
+    """terminating condition for agent occupying same position as an object
+
+    Args:
+        state (`State`):
+        action (`Actions`):
+        next_state (`State`):
+        object_type (`Type[GridObject]`):
+
+    Returns:
+        bool: True if next_state agent is on object of type object_type
+    """
+    return isinstance(next_state.grid[next_state.agent.position], object_type)
+
+
+def reach_goal(state: State, action: Actions, next_state: State,) -> bool:
     """terminating condition for Agent reaching the Goal
 
     Args:
@@ -22,4 +39,4 @@ def reach_goal(
     Returns:
         bool: True if next_state agent is on goal
     """
-    return isinstance(next_state.grid[next_state.agent.position], Goal)
+    return overlap(state, action, next_state, object_type=Goal)
