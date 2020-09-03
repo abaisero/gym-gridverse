e83ba3c89d5c9dbe85a464e6cb31fe36e3cc132a
abaisero <andrea.baisero@gmail.com>
2020-09-03 14:50:17 -0400

    add DefaultObservationRepresentation implementation

--- gym_gridverse/representations/observation_representations.py
+++ gym_gridverse/representations/observation_representations.py
@@ -1,7 +1,11 @@
 from typing import Dict
 
 import numpy as np
+
+from gym_gridverse.grid_object import NoneGridObject
+from gym_gridverse.observation import Observation
 from gym_gridverse.representations.representation import Representation
+from gym_gridverse.spaces import ObservationSpace
 
 
 class DefaultObservationRepresentation(Representation):
@@ -11,15 +15,83 @@ class DefaultObservationRepresentation(Representation):
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, observation_space: ObservationSpace):
+        self.observation_space = observation_space
 
     @property
     def space(self) -> Dict[str, np.ndarray]:
-        pass
+        max_type_index = max(
+            object_type.type_index
+            for object_type in self.observation_space.object_types
+        )
+        max_state_index = max(
+            object_type.num_states()
+            for object_type in self.observation_space.object_types
+        )
+        max_color_value = max(
+            color.value for color in self.observation_space.colos
+        )
 
-    def convert(self, x) -> Dict[str, np.ndarray]:
-        pass
+        grid_array = np.array(
+            [
+                [
+                    [max_type_index, max_state_index, max_color_value]
+                    for x in range(self.observation_space.grid_shape.width)
+                ]
+                for y in range(self.observation_space.grid_space.height)
+            ]
+        )
+        agent_array = np.array(
+            [max_type_index, max_state_index, max_color_value]
+        )
+        return {'grid': grid_array, 'agent': agent_array}
+
+    def convert(self, observation: Observation) -> Dict[str, np.ndarray]:
+        # TODO either ignore pylint or avoid specialization of signature input
+
+        # TODO make sure input observation fits space?
+
+        agent_obj_array = np.array(
+            [
+                observation.agent.obj.type_index,
+                observation.agent.obj.state_index,
+                observation.agent.obj.color.value,
+            ]
+        )
+
+        grid_array_object_channels = np.array(
+            [
+                [
+                    [
+                        observation.grid[y, x].type_index,
+                        observation.grid[y, x].state_index,
+                        observation.grid[y, x].color.value,
+                    ]
+                    for x in range(self.observation_space.grid_shape.width)
+                ]
+                for y in range(self.observation_space.grid_space.height)
+            ]
+        )
+        none_grid_object = NoneGridObject()
+        grid_array_agent_channels = np.array(
+            [
+                [
+                    [
+                        none_grid_object.type_index,  # pylint: disable=no-member
+                        none_grid_object.state_index,
+                        none_grid_object.color.value,
+                    ]
+                    for x in range(self.observation_space.grid_shape.width)
+                ]
+                for y in range(self.observation_space.grid_space.height)
+            ]
+        )
+        grid_array_agent_channels[observation.agent.position] = agent_obj_array
+
+        grid_array = np.concatenate(
+            (grid_array_agent_channels, grid_array_object_channels), axis=0
+        )
+        return {'grid': grid_array, 'agent': agent_obj_array}
 
 
 class CompactObservationRepresentation(Representation):
