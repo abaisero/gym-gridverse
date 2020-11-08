7e1ba5974afb26496db44cdc53837ebaa2422480
abaisero <andrea.baisero@gmail.com>
2020-11-07 22:54:05 -0500

    integrate factory_yaml code and default schema into library package

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     url='https://github.com/abaisero/gym-gridverse',
     packages=['gym_gridverse'],
     install_requires=['gym', 'more_itertools', 'numpy', 'termcolor', 'yamale'],
+    package_data={'gym_gridverse': ['envs/assets/schema.yaml']},
     scripts=['scripts/gv_gym_interface.py', 'scripts/gv_yaml.py'],
     license='MIT',
 )
