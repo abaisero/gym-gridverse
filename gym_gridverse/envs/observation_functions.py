fbadaa8e36d25a7b2d9f9766179e5da37c31fcf1
abaisero <andrea.baisero@gmail.com>
2020-11-17 22:08:20 -0500

    export visibility functions in their own module

--- gym_gridverse/envs/observation_functions.py
+++ gym_gridverse/envs/observation_functions.py
@@ -1,17 +1,19 @@
-import math
-from functools import lru_cache, partial
-from typing import Iterator, List, Optional
+from functools import partial
+from typing import Optional
 
-import more_itertools as mitt
-import numpy as np
 import numpy.random as rnd
 from typing_extensions import Protocol  # python3.7 compatibility
 
-from gym_gridverse.geometry import Area, Orientation, Position
+from gym_gridverse.envs.visibility_functions import (
+    VisibilityFunction,
+    minigrid_visibility,
+    raytracing_visibility,
+    stochastic_raytracing_visibility,
+)
+from gym_gridverse.geometry import Orientation
 from gym_gridverse.grid_object import Hidden
-from gym_gridverse.info import Agent, Grid
+from gym_gridverse.info import Agent
 from gym_gridverse.observation import Observation
-from gym_gridverse.rng import get_gv_rng_if_none
 from gym_gridverse.spaces import ObservationSpace
 from gym_gridverse.state import State
 
@@ -20,17 +22,6 @@ class ObservationFunction(Protocol):
     def __call__(
         self, state: State, *, rng: Optional[rnd.Generator] = None
     ) -> Observation:
-        ...
-
-
-class VisibilityFunction(Protocol):
-    def __call__(
-        self,
-        grid: Grid,
-        position: Position,
-        *,
-        rng: Optional[rnd.Generator] = None,
-    ) -> np.array:
         ...
 
 
@@ -82,123 +73,6 @@ def from_visibility(
     return Observation(observation_grid, observation_agent)
 
 
-def minigrid_visibility(
-    grid: Grid,
-    position: Position,
-    *,
-    rng: Optional[rnd.Generator] = None,
-) -> np.ndarray:
-
-    visibility = np.zeros((grid.height, grid.width), dtype=bool)
-    visibility[position.y, position.x] = True  # agent
-
-    for y in range(grid.height - 1, -1, -1):
-        for x in range(grid.width - 1):
-            if visibility[y, x] and grid[y, x].transparent:
-                visibility[y, x + 1] = True
-                if y > 0:
-                    visibility[y - 1, x] = True
-                    visibility[y - 1, x + 1] = True
-
-        for x in range(grid.width - 1, 0, -1):
-            if visibility[y, x] and grid[y, x].transparent:
-                visibility[y, x - 1] = True
-                if y > 0:
-                    visibility[y - 1, x] = True
-                    visibility[y - 1, x - 1] = True
-
-    return visibility
-
-
-# TODO test this
-def ray_positions(
-    start_pos: Position, area: Area, *, radians: float, step_size: float
-) -> Iterator[Position]:
-    y, x = float(start_pos.y), float(start_pos.x)
-    dy = -math.sin(radians)
-    dx = math.cos(radians)
-
-    pos = start_pos
-    while area.contains(pos):
-        yield pos
-
-        y += step_size * dy
-        x += step_size * dx
-        pos = Position(round(y), round(x))
-
-
-# TODO test this
-@lru_cache()
-def rays_positions(start_pos: Position, area: Area) -> List[List[Position]]:
-    rays: List[List[Position]] = []
-
-    for degrees in range(360):
-        # conversion to radians
-        radians = degrees * math.pi / 180.0
-        ray = ray_positions(start_pos, area, radians=radians, step_size=0.01)
-        ray = mitt.unique_justseen(ray)
-        rays.append(list(ray))
-
-    return rays
-
-
-def raytracing_visibility(
-    grid: Grid, position: Position, *, rng: Optional[rnd.Generator] = None
-) -> np.ndarray:
-
-    area = Area((0, grid.height - 1), (0, grid.width - 1))
-    rays = rays_positions(position, area)
-
-    counts_num = np.zeros((area.height, area.width), dtype=int)
-    counts_den = np.zeros((area.height, area.width), dtype=int)
-
-    for ray in rays:
-        light = True
-        for pos in ray:
-            if light:
-                counts_num[pos.y, pos.x] += 1
-
-            counts_den[pos.y, pos.x] += 1
-
-            light = light and grid[pos].transparent
-
-    visibility = counts_num > 0  # at least one ray makes it
-    # visibility = counts_num > 0.5 * counts_den # half of the rays make it
-    # visibility = counts_num > 0.1 * counts_den  # 10% of the rays make it
-    # visibility = counts_num > 1  # at least 2 rays make it
-
-    return visibility
-
-
-def stochastic_raytracing_visibility(  # TODO add test
-    grid: Grid,
-    position: Position,
-    *,
-    rng: Optional[rnd.Generator] = None,
-) -> np.ndarray:
-    rng = get_gv_rng_if_none(rng)
-
-    area = Area((0, grid.height - 1), (0, grid.width - 1))
-    rays = rays_positions(position, area)
-
-    counts_num = np.zeros((area.height, area.width), dtype=int)
-    counts_den = np.zeros((area.height, area.width), dtype=int)
-
-    for ray in rays:
-        light = True
-        for pos in ray:
-            if light:
-                counts_num[pos.y, pos.x] += 1
-
-            counts_den[pos.y, pos.x] += 1
-
-            light = light and grid[pos].transparent
-
-    probs = np.nan_to_num(counts_num / counts_den)
-    visibility = probs <= rng.random(probs.shape)
-    return visibility
-
-
 minigrid_observation = partial(
     from_visibility, visibility_function=minigrid_visibility
 )
@@ -208,7 +82,8 @@ raytracing_observation = partial(
 )
 
 stochastic_raytracing_observation = partial(
-    from_visibility, visibility_function=raytracing_visibility
+    from_visibility,
+    visibility_function=stochastic_raytracing_visibility,
 )
 
 
---
+++ gym_gridverse/envs/visibility_functions.py
@@ -0,0 +1,143 @@
+import math
+from functools import lru_cache
+from typing import Iterator, List, Optional
+
+import more_itertools as mitt
+import numpy as np
+import numpy.random as rnd
+from typing_extensions import Protocol  # python3.7 compatibility
+
+from gym_gridverse.geometry import Area, Position
+from gym_gridverse.info import Grid
+from gym_gridverse.rng import get_gv_rng_if_none
+
+
+class VisibilityFunction(Protocol):
+    def __call__(
+        self,
+        grid: Grid,
+        position: Position,
+        *,
+        rng: Optional[rnd.Generator] = None,
+    ) -> np.array:
+        ...
+
+
+def minigrid_visibility(
+    grid: Grid,
+    position: Position,
+    *,
+    rng: Optional[rnd.Generator] = None,  # pylint: disable = unused-argument
+) -> np.ndarray:
+
+    visibility = np.zeros((grid.height, grid.width), dtype=bool)
+    visibility[position.y, position.x] = True  # agent
+
+    for y in range(grid.height - 1, -1, -1):
+        for x in range(grid.width - 1):
+            if visibility[y, x] and grid[y, x].transparent:
+                visibility[y, x + 1] = True
+                if y > 0:
+                    visibility[y - 1, x] = True
+                    visibility[y - 1, x + 1] = True
+
+        for x in range(grid.width - 1, 0, -1):
+            if visibility[y, x] and grid[y, x].transparent:
+                visibility[y, x - 1] = True
+                if y > 0:
+                    visibility[y - 1, x] = True
+                    visibility[y - 1, x - 1] = True
+
+    return visibility
+
+
+# TODO test this
+def ray_positions(
+    start_pos: Position, area: Area, *, radians: float, step_size: float
+) -> Iterator[Position]:
+    y, x = float(start_pos.y), float(start_pos.x)
+    dy = -math.sin(radians)
+    dx = math.cos(radians)
+
+    pos = start_pos
+    while area.contains(pos):
+        yield pos
+
+        y += step_size * dy
+        x += step_size * dx
+        pos = Position(round(y), round(x))
+
+
+# TODO test this
+@lru_cache()
+def rays_positions(start_pos: Position, area: Area) -> List[List[Position]]:
+    rays: List[List[Position]] = []
+
+    for degrees in range(360):
+        # conversion to radians
+        radians = degrees * math.pi / 180.0
+        ray = ray_positions(start_pos, area, radians=radians, step_size=0.01)
+        ray = mitt.unique_justseen(ray)
+        rays.append(list(ray))
+
+    return rays
+
+
+def raytracing_visibility(
+    grid: Grid,
+    position: Position,
+    *,
+    rng: Optional[rnd.Generator] = None,  # pylint: disable=unused-argument
+) -> np.ndarray:
+
+    area = Area((0, grid.height - 1), (0, grid.width - 1))
+    rays = rays_positions(position, area)
+
+    counts_num = np.zeros((area.height, area.width), dtype=int)
+    counts_den = np.zeros((area.height, area.width), dtype=int)
+
+    for ray in rays:
+        light = True
+        for pos in ray:
+            if light:
+                counts_num[pos.y, pos.x] += 1
+
+            counts_den[pos.y, pos.x] += 1
+
+            light = light and grid[pos].transparent
+
+    visibility = counts_num > 0  # at least one ray makes it
+    # visibility = counts_num > 0.5 * counts_den # half of the rays make it
+    # visibility = counts_num > 0.1 * counts_den  # 10% of the rays make it
+    # visibility = counts_num > 1  # at least 2 rays make it
+
+    return visibility
+
+
+def stochastic_raytracing_visibility(  # TODO add test
+    grid: Grid,
+    position: Position,
+    *,
+    rng: Optional[rnd.Generator] = None,
+) -> np.ndarray:
+    rng = get_gv_rng_if_none(rng)
+
+    area = Area((0, grid.height - 1), (0, grid.width - 1))
+    rays = rays_positions(position, area)
+
+    counts_num = np.zeros((area.height, area.width), dtype=int)
+    counts_den = np.zeros((area.height, area.width), dtype=int)
+
+    for ray in rays:
+        light = True
+        for pos in ray:
+            if light:
+                counts_num[pos.y, pos.x] += 1
+
+            counts_den[pos.y, pos.x] += 1
+
+            light = light and grid[pos].transparent
+
+    probs = np.nan_to_num(counts_num / counts_den)
+    visibility = probs <= rng.random(probs.shape)
+    return visibility
