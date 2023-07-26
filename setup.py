fbe84dcae69c198a5de937a9c9958356b9f150f8
abaisero <andrea.baisero@gmail.com>
2023-07-26 13:43:15 -0400

    fix pyglet version to avoid glPushMatrix bug

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'pyglet',
+    'pyglet==1.5.27',
     'gym[classic_control]<=0.21.0',
     'imageio',
     'imageio-ffmpeg',
